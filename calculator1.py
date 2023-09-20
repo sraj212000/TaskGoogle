2425279b900e90450e61815790ccc8c60724fe48
sraj212000 <sraj212000@gmail.com>
2023-09-20 17:02:01 +0530

    Initial Commit

---
+++ calculator1.py
@@ -0,0 +1,10 @@
+# Calculator on two variables a and b
+a = int(input("Enter an integer as input:"))
+b = int(input("Enter second integer as input"))
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a*b)
+print("Division", a/b)
+print("Exponential", a**b)
+print("quotient on a is divided by b", a//b)
+print("remainder on a is divided by b", a%b)
